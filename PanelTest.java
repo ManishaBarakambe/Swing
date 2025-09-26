fb0fadd44664dfb6dbe1a5261e7cc4b44006b7e3
ManishaBarakambe <manishabarkambe3591@gmail.com>
2025-09-26 10:14:44 +0530

    Add files via upload

---
+++ PanelTest.java
@@ -0,0 +1,33 @@
+package swing;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class PanelTest extends JFrame {
+	private JButton b,b1,b2,b3,b4;
+	private JLabel l;
+	public PanelTest() {
+		l=new JLabel("panel label");
+		b=new JButton("button1");
+		b1=new JButton("button2");
+		b2=new JButton("button3");
+		b3=new JButton("button4");
+		b4=new JButton("button5");
+		
+		JPanel p=new JPanel();
+		p.add(b);
+		p.add(b1);
+		p.add(b2);
+		p.add(b3);
+		p.add(b4);
+		p.add(l);
+		p.setBackground(Color.red);
+		add(p);
+		setSize(300,300);
+		show();
+	}
+	
+	public static void main(String[] args) {
+		PanelTest p=new PanelTest();
+	}
+
+}
---
+++ VaccinationDetails.java
@@ -0,0 +1,20 @@
+package swing;
+
+import java.awt.event.*; 
+import javax.swing.*;  
+import java.awt.*; 
+public class VaccinationDetails extends JFrame implements ActionListener {
+	public  VaccinationDetails () {
+
+        setTitle("Vaccination Details");
+        setSize(500, 400); 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
